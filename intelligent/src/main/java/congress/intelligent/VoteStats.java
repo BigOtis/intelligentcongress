f92dfef7cb95f860b26f57a11ea77d2041142fb6
Phillip Lopez <pgl5711@rit.edu>
2017-02-04 00:12:49 -0500

    Added a ton of vote data

---
+++ intelligent/src/main/java/congress/intelligent/VoteStats.java
@@ -0,0 +1,52 @@
+package congress.intelligent;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import utils.JSONUtils;
+
+public class VoteStats {
+	
+	public static void main( String[] args ){
+		
+		VoteStats vs = new VoteStats();
+		vs.printStats("s113", "2013");
+		
+	}
+
+	public void printStats(String congress, String year){
+		
+		Map<String, Integer> billVotesCount = new HashMap<>();
+		File billsDir = new File(congress + "_" + year + "_bills");
+		File votesDir = new File(congress + "_" + year + "_votes");
+
+		for(File voteFile : votesDir.listFiles()){
+			JSONObject vote = JSONUtils.getJSONObject(voteFile);
+			JSONObject voteBill = JSONUtils.getVoteBill(vote);
+			if(voteBill != null){
+				putPlus(billVotesCount, voteBill.getInt("number")+"");
+			}
+		}
+		
+		printMap(billVotesCount);
+	}
+	
+	public void printMap(Map<?,?> map){
+		for(Object key : map.keySet()){
+			System.out.println(key + " : " + map.get(key));
+		}
+	}
+	
+	public void putPlus(Map<String, Integer> map, String key){
+		if(map.get(key) == null){
+			map.put(key, 1);
+		}
+		else{
+			map.put(key,map.get(key)+1);
+		}
+	}
+	
+}
