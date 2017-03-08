86a3150c056c58858e241a375275caa98d29396a
Phillip Lopez <pgl5711@rit.edu>
2017-03-08 17:54:27 -0500

    Added script to load legislator data

--- intelligent/src/main/java/congress/mongodb/load/ImportLegislatorData.java
+++ intelligent/src/main/java/congress/mongodb/load/ImportLegislatorData.java
@@ -1,5 +1,28 @@
 package congress.mongodb.load;
 
+import java.io.File;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mongodb.client.MongoCollection;
+
+import congress.mongo.facade.MongoDBFacade;
+import utils.JSONUtils;
+
 public class ImportLegislatorData {
 
+	public static void main(String args[]){
+		
+		System.out.println("Adding legislators to DB from legislators.json...");
+		MongoDBFacade mongo = MongoDBFacade.getInstance();
+		MongoCollection<Document> collection = mongo.db.getCollection("Legislators");
+		JSONArray legislators = new JSONArray(JSONUtils.getFileAsString(new File("legislators.json")));
+		for(int i = 0; i < legislators.length(); i++){
+			JSONObject legislator = legislators.getJSONObject(i);
+			collection.insertOne(Document.parse(legislator.toString()));
+		}
+	}
+	
 }
